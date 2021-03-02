ec00fa9d13fc3fe9e94765fdd7578e9c687c1e68
Mr. Habib <78511534+mr-habib@users.noreply.github.com>
2021-03-02 08:41:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Firstname, L
+# mm/dd/yyyy
+# Good descriptive title
+# Note: I will be taking a look at your comments for this assignment. I won't be marking them, but I'll be providing feedback on them as a good way to practice!
+
+# Implement binary search here
+# Return the position of the element if it's in the array
+# Return False if item is not in the array
+def binary_search(arr, item):
+  pass
